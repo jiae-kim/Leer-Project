ac0ccc9a452cc3c0375eadfd99b886272e085a32
jiae-kim <99962500+jiae-kim@users.noreply.github.com>
2022-07-29 10:20:36 +0900

    [김지애] AdminProductListController
    상품전체조회

---
+++ src/main/java/com/leer/product/controller/AdminProductListController.java
@@ -0,0 +1,50 @@
+package com.leer.product.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.leer.product.model.service.AdminProductService;
+import com.leer.product.model.vo.Product;
+
+/**
+ * Servlet implementation class AdminProductListController
+ */ 
+@WebServlet("/adProList.do") 
+public class AdminProductListController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminProductListController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    
+    /* [제품관리 - 상품조회]
+     * 상품 전체조회 기능
+     * 작성자 김지애
+     */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArrayList<Product> list = new AdminProductService().selectProductList();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
