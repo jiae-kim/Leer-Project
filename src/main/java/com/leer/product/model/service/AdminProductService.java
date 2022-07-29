39b71655894d9ec814e5b4908824fabcbbf59869
jiae-kim <99962500+jiae-kim@users.noreply.github.com>
2022-07-29 10:21:16 +0900

    [김지애] AdminProductService
    상품 Service

---
+++ src/main/java/com/leer/product/model/service/AdminProductService.java
@@ -0,0 +1,50 @@
+package com.leer.product.model.service;
+
+import static com.leer.common.JDBCTemplate.close;
+import static com.leer.common.JDBCTemplate.getConnection;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import com.leer.product.model.dao.ProductDao;
+import com.leer.product.model.vo.Inquiry;
+import com.leer.product.model.vo.Product;
+
+public class AdminProductService {
+
+	public ArrayList<Product> selectProductList(){
+		Connection conn = getConnection();
+		
+		ArrayList<Product> list = new ProductDao().selectProductList(conn);
+		
+		close(conn);
+		return list;
+	}
+	
+	public ArrayList<Product> selectProductListOld(){
+		Connection conn = getConnection();
+		
+		ArrayList<Product> list = new ProductDao().selectProductListOld(conn);
+		
+		close(conn);
+		return list;
+	}
+	
+	public Product selectProductDetail(String pCode) {
+		Connection conn = getConnection();
+		
+		Product p = new ProductDao().selectProductDetail(conn, pCode);
+		
+		close(conn);
+		return p;
+	}
+	
+	public ArrayList<Inquiry> selectInquiryList(String pCode){
+		Connection conn = getConnection();
+		
+		ArrayList<Inquiry> list = new ProductDao().selectInquiryList(conn, pCode);
+		
+		close(conn);
+		return list;
+	}
+}
