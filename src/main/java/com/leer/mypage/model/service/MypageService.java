5adb72ba857e8cd32d6514f6be4d980334fb4f3a
pandabaeung <69721328+barti971219@users.noreply.github.com>
2022-07-30 19:52:50 +0900

    [노희영] 사용자 마이페이지 cart select
    - 카트 조회

--- src/main/java/com/leer/mypage/model/service/MypageService.java
+++ src/main/java/com/leer/mypage/model/service/MypageService.java
@@ -1,8 +1,10 @@
 package com.leer.mypage.model.service;
 
+import static com.leer.common.JDBCTemplate.close;
 import static com.leer.common.JDBCTemplate.getConnection;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 
 import com.leer.mypage.model.dao.MypageDao;
 import com.leer.mypage.model.vo.Cart;
@@ -14,7 +16,20 @@ public class MypageService {
 		Connection conn = getConnection();
 		
 		int result = new MypageDao().insertProductCart(conn, c);
-		return 2;
+		
+		close(conn);
+		return result;
+		
+	}
+	
+	public ArrayList<Cart> selectCartList(int memNo) {
+		
+		Connection conn = getConnection();
+		
+		ArrayList<Cart> list = new MypageDao().selectCartList(conn, memNo);
+		
+		close(conn);
+		return list; 
 	}
 
 }
--- src/main/java/com/leer/product/controller/ProductInsertCartController.java
+++ src/main/java/com/leer/product/controller/ProductInsertCartController.java
@@ -1,6 +1,7 @@
 package com.leer.product.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.leer.mypage.model.service.MypageService;
 import com.leer.mypage.model.vo.Cart;
 
 /**
@@ -29,16 +31,30 @@ public class ProductInsertCartController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*int memNo = request.getSession.getAttribute("loginUser")*/
 		String pCode = request.getParameter("pNo");	
 		int orCycle = Integer.parseInt(request.getParameter("shipping_cycle"));
 		int amount = Integer.parseInt(request.getParameter("amount"));
 		
+		
 		Cart c = new Cart();
+		/*c.setMemNo(memNo);*/
+		c.setMemNo(3);
 		c.setpCode(pCode);
 		c.setOrCycle(orCycle);
 		c.setAmount(amount);
 		
-		int result = new MyPageService().insertProductCart(c);
+		int result = new MypageService().insertProductCart(c);
+		
+		ArrayList<Cart> list  = new MypageService().selectCartList(c.getMemNo());
+		
+		if(result > 0) {
+			request.setAttribute("list", list);
+			request.getRequestDispatcher("views/mypage/mypageCartList.jsp").forward(request, response);
+		}else { 
+			
+		}
+		
 	}
 
 	/**
