3defbb55de022ca909d2f7ec6873c9578d8289fb
jinibam <qjawls6457@gmail.com>
2022-07-30 17:13:04 +0900

    common.model.vo.Attachment 생성

---
+++ src/main/java/com/leer/common/model/vo/Attachment.java
@@ -0,0 +1,124 @@
+package com.leer.common.model.vo;
+
+import java.sql.Date;
+
+public class Attachment {
+	private int fileNo;
+	private String refCode;
+	private int refBno;
+	private String originName;
+	private String changeName;
+	private String filePath;
+	private Date enrillDate;
+	private String fileYn;
+	private int fileLevel;
+	private String fileInfo;
+	
+	public Attachment() {}
+	
+	public Attachment(int fileNo, String refCode, int refBno, String originName, String changeName, String filePath,
+			Date enrillDate, String fileYn, int fileLevel, String fileInfo) {
+		super();
+		this.fileNo = fileNo;
+		this.refCode = refCode;
+		this.refBno = refBno;
+		this.originName = originName;
+		this.changeName = changeName;
+		this.filePath = filePath;
+		this.enrillDate = enrillDate;
+		this.fileYn = fileYn;
+		this.fileLevel = fileLevel;
+		this.fileInfo = fileInfo;
+	}
+
+	public int getFileNo() {
+		return fileNo;
+	}
+
+	public void setFileNo(int fileNo) {
+		this.fileNo = fileNo;
+	}
+
+	public String getRefCode() {
+		return refCode;
+	}
+
+	public void setRefCode(String refCode) {
+		this.refCode = refCode;
+	}
+
+	public int getRefBno() {
+		return refBno;
+	}
+
+	public void setRefBno(int refBno) {
+		this.refBno = refBno;
+	}
+
+	public String getOriginName() {
+		return originName;
+	}
+
+	public void setOriginName(String originName) {
+		this.originName = originName;
+	}
+
+	public String getChangeName() {
+		return changeName;
+	}
+
+	public void setChangeName(String changeName) {
+		this.changeName = changeName;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public Date getEnrillDate() {
+		return enrillDate;
+	}
+
+	public void setEnrillDate(Date enrillDate) {
+		this.enrillDate = enrillDate;
+	}
+
+	public String getFileYn() {
+		return fileYn;
+	}
+
+	public void setFileYn(String fileYn) {
+		this.fileYn = fileYn;
+	}
+
+	public int getFileLevel() {
+		return fileLevel;
+	}
+
+	public void setFileLevel(int fileLevel) {
+		this.fileLevel = fileLevel;
+	}
+
+	public String getFileInfo() {
+		return fileInfo;
+	}
+
+	public void setFileInfo(String fileInfo) {
+		this.fileInfo = fileInfo;
+	}
+
+	@Override
+	public String toString() {
+		return "Attachment [fileNo=" + fileNo + ", refCode=" + refCode + ", refBno=" + refBno + ", originName="
+				+ originName + ", changeName=" + changeName + ", filePath=" + filePath + ", enrillDate=" + enrillDate
+				+ ", fileYn=" + fileYn + ", fileLevel=" + fileLevel + ", fileInfo=" + fileInfo + "]";
+	}
+	
+	
+	
+	
+}
--- src/main/java/com/leer/community/controller/InsertBoardController.java
+++ src/main/java/com/leer/community/controller/InsertBoardController.java
@@ -1,16 +1,25 @@
 package com.leer.community.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+
+import com.leer.common.MyFileRenamePolicy;
+import com.leer.community.model.vo.ComuBoard;
+import com.leer.member.model.vo.Member;
+import com.oreilly.servlet.MultipartRequest;
 
 /**
  * Servlet implementation class InsertBoardController
  */
-@WebServlet("/InsertBoardController")
+@WebServlet("/comuinsert.bo")
 public class InsertBoardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,7 +35,36 @@ public class InsertBoardController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 		
+		HttpSession session = request.getSession();
+		
+		if(ServletFileUpload.isMultipartContent(request)) {
+			int maxSize = 10 * 1024 * 1024;
+			
+			String savePath = session.getServletContext().getRealPath("/resources/comu_upfiles/");
+			
+			MultipartRequest multiRequest = new MultipartRequest(request, savePath, maxSize, "UTF-8", new MyFileRenamePolicy());
+			
+			String category = multiRequest.getParameter("category");
+//			String tag[] = multiRequest.getParameterValues("tag");
+			String title = multiRequest.getParameter("title");
+			String content = multiRequest.getParameter("content");
+			int nickname = ((Member)session.getAttribute("loginUser")).getMemNo();
+			
+			ComuBoard c = new ComuBoard();
+			c.setCategoryNo(category);
+//			c.setTag(tag[]);
+			c.setTitle(title);
+			c.setContent("content");
+			c.setMemNo(String.valueOf(nickname));
+			
+			Attachment at = null;
+			
+			
+			
+			
+		}
 	}
 
 	/**
