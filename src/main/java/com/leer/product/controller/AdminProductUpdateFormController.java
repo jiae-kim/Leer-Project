fe1df13a319b04e441f9f64d37b7b6facd6029b4
jiae-kim <99962500+jiae-kim@users.noreply.github.com>
2022-07-31 21:56:50 +0900

    [김지애] 상품전체조회 수정버튼
    수정버튼 클릭 시 수정페이지 요청

---
+++ src/main/java/com/leer/product/controller/AdminProductUpdateFormController.java
@@ -0,0 +1,50 @@
+package com.leer.product.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.leer.product.model.service.AdminProductService;
+import com.leer.product.model.vo.Product;
+
+/**
+ * Servlet implementation class AdminProductUpdateFormController
+ */
+@WebServlet("/adProUpdate.do")
+public class AdminProductUpdateFormController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminProductUpdateFormController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 상품전체조회 페이지에서 수정버튼 클릭 시 수정페이지 요청
+		
+		// PRODUCT 테이블에는 pNo이 없기 때문에 상품코드를 넘기자
+		String pCode = request.getParameter("pcode");
+		Product p = new AdminProductService().selectProduct(pCode);
+		
+		request.setAttribute("pcode", p);
+		request.getRequestDispatcher("views/admin_main/product.adminProductDetail.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
